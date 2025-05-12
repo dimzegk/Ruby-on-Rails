6a1fe4c2d562ff0dc14667778bfeaeabc6496838
Dimitris <dimzegk@gmail.com>
2025-05-12 23:39:05 +0300

    Add feature specs for posts' infinite scroll functionality

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -66,7 +66,7 @@ end
 
 context '#update_pagination_partial_path' do
   it "returns an update_pagination partial's path" do
-    posts = double('posts', :next_page => 2)
+    posts = double('posts', next_page: 2)
     assign(:posts, posts)
     expect(helper.update_pagination_partial_path).to(
       eq 'posts/posts_pagination_page/update_pagination'
@@ -74,10 +74,11 @@ context '#update_pagination_partial_path' do
   end
 
   it "returns a remove_pagination partial's path" do
-    posts = double('posts', :next_page => nil)
+    posts = double('posts', next_page: nil)
     assign(:posts, posts)
     expect(helper.update_pagination_partial_path).to(
       eq 'posts/posts_pagination_page/remove_pagination'
     )
   end
+end
 end
